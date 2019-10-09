cd0a0d21542c076254eb4ebd559b8626113b8084
Jae <ujpyon@gmail.com>
2019-10-09 09:59:15 -0400

    Remove debuggers in preparation to push to Heroku

---
+++ frontend/utils/comment_api_util.js
@@ -0,0 +1,37 @@
+export const getAllComments = () => {
+  return (
+    $.ajax({
+      method: "GET",
+      url: "/api/comments",
+    })
+  );
+}
+
+// --TEST CODE: RETRIEVE A SPECIFIC PROJECT'S COMMENTS--
+// export const getProjectComments = (id) => {
+//   return (
+//     $.ajax({
+//       method: "GET",
+//       url: `/api/projects/${id}/comments`,
+//     })
+//   );
+// }
+
+export const postComment = (comment) => {
+  return (
+    $.ajax({
+      method: "POST",
+      url: "/api/comments",
+      data: { comment },
+    })
+  );
+}
+
+export const deleteComment = (id) => {
+  return (
+    $.ajax({
+      method: "DELETE",
+      url: `/api/comments${id}`,
+    })
+  );
+}
